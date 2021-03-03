a39a71f370b2e01b7506efee701d9b365f563fee
GitHub User Name <rom644@gmail.com>
2021-03-03 16:49:21 +0200

    merge fix .

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import Login from './components/Authentication/Login/Login';
 import Register from './components/Authentication/Register/Register';
 import Map from './components/Posts/Map/Map';
-import './App.css';
 import Modal from 'react-modal';
 
 Modal.setAppElement('#root');
@@ -9,7 +8,7 @@ Modal.setAppElement('#root');
 function App() {
   return (
     <div className='h-full'>
-      <Register/>
+      <Register />
     </div>
   );
 }
