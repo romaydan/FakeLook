2560d4b41054fbc789dbdc7ac5b6a87f0dbc4eb4
GitHub User Name <rom644@gmail.com>
2021-03-07 12:24:20 +0200

    TESTING

--- client/src/components/Routers/SocialRouter.js
+++ client/src/components/Routers/SocialRouter.js
@@ -7,8 +7,8 @@ const SocialRouter = props => {
     const [userId, setUserId] = useState("")
     useEffect(() => {
         setUserId("cc19e705-41fc-4037-9a7b-c5b488f87a53");
-        // setUserId("2ee1ab6f-b701-422e-8244-161335b89fbb");
-        // setUserId("1716dc65-485f-4447-92ca-d780f708cb1d");
+        //setUserId("2ee1ab6f-b701-422e-8244-161335b89fbb"); //romaydan
+        // setUserId("1716dc65-485f-4447-92ca-d780f708cb1d"); //elior
     }, [])
     return (
         <Switch>
