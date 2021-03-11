55308b3c9c9f8585fd3894bbc41668516071caa8
GitHub User Name <rom644@gmail.com>
2021-03-11 12:08:24 +0200

    Gateway update.

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const env = require('dotenv');
 const cors = require('cors');
+
 const authRouter = require('./routers/authentication.router');
 const postRouter = require('./routers/posts.router');
 const friendsRouter = require('./routers/friends.router');
+const groupsRouter = require('./routers/groups.router');
 
 env.config();
 
-const PORT = process.env.PORT || 5010;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
@@ -17,6 +20,7 @@ app.use(express.json())
 app.use('/auth', authRouter);
 app.use('/posts', postRouter);
 app.use('/friends', friendsRouter);
+app.use('/groups', groupsRouter);
 
 app.listen(PORT, () => {
     console.log(`Now listening on port ${PORT}...`);
