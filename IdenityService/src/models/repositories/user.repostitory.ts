d3a32dd077504aeacc1446d79fc0b493bf3eb003
GitHub User Name <rom644@gmail.com>
2021-03-14 13:39:47 +0200

    Rom's commit.

---
+++ IdenityService/src/models/repositories/user.repostitory.ts
@@ -0,0 +1,75 @@
+import { injectable } from 'inversify';
+import { Op } from 'sequelize';
+import * as uuid from 'uuid';
+
+import Address from '../address.model';
+import IAddress from '../../interfaces/IAddress';
+import IUser from '../../interfaces/IUser';
+import IUserRepository from '../../interfaces/user-repository.interface';
+import User from '../user.model';
+
+@injectable()
+export default class UserRepository implements IUserRepository {
+  constructor() {}
+  getUsersByName(name: string): Promise<IUser[]> {
+    name = name.toLowerCase();
+    return User.findAll({ where: { name: { [Op.like]: `%${name}%` } } });
+  }
+  getUsersById(userIds: string[]): Promise<IUser[]> {
+    return User.findAll({ where: { authId: { [Op.in]: userIds } }, include: [Address] });
+  }
+
+  async addUser(user: IUser, address: IAddress): Promise<IUser> {
+    user.name = user.name.toLowerCase();
+    console.log('user.name', user.name);
+    return User.create({ id: uuid.v4(), ...user, address: { id: uuid.v4(), ...address } }, { include: Address });
+  }
+  async getUserByAuthId(id: string): Promise<IUser> {
+    const userFromDb = await User.findOne({
+      where: { authId: id },
+      include: [Address],
+    });
+    if (userFromDb === null) {
+      console.log('USER NOT FOUND');
+      throw new Error('user not found');
+    } else {
+      console.log('userFormDb', userFromDb);
+      return userFromDb;
+    }
+  }
+  async removeUserById(userId: string): Promise<boolean> {
+    const user = await this.findUserbyId(userId);
+    try {
+      await user.destroy();
+      return true;
+    } catch (error) {
+      throw new Error('user was not deleted');
+    }
+  }
+  async updateUser(id: string, user: IUser, address: IAddress): Promise<boolean> {
+    console.log('here');
+
+    // const { id, firstName, lastName, birthDate } = user;
+    user.address = address;
+    console.log('user', user);
+    const [addressRowsAffected, addresses] = await Address.update(address, {
+      where: { userId: id },
+    });
+    const [userRowsAffected, users] = await User.update(user, {
+      where: { authId: id },
+    });
+    console.log('users', users);
+    return addressRowsAffected + userRowsAffected > 0 ? true : false;
+  }
+  private async findUserbyId(id): Promise<User> {
+    const userFromDb = await User.findOne({
+      where: { authId: id },
+      include: [Address],
+    });
+    if (userFromDb === null) {
+      throw new Error('user not found');
+    } else {
+      return userFromDb;
+    }
+  }
+}
